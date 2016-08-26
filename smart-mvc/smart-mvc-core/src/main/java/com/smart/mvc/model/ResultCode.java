725333d9185e367a28008a20270caef76fc0adaa
Joe <a466350665@qq.com>
2016-08-26 14:38:08 +0800

    基础类修改还原

--- smart-mvc/smart-mvc-core/src/main/java/com/smart/mvc/model/ResultCode.java
+++ smart-mvc/smart-mvc-core/src/main/java/com/smart/mvc/model/ResultCode.java
@@ -1,15 +1,8 @@
 package com.smart.mvc.model;
 
-/**
- * <b>Description:结果状态码</b><br>
- * @author 唐海洋
- * @version 1.0 2016-8-25 下午10:21:37
- */
-public final class ResultCode{
+public final class ResultCode {
 	
-	public final static Integer SUCCESS = 0;// 成功
+	public final static String SUCCESS = "0000";// 成功
 
-	public final static Integer WARN = 1;// 警告
-
-	public final static Integer ERROR = 2;// 错误
+	public final static String ERROR = "9999";// 未知错误
 }
