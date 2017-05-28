e0c1c02f072bdf0c81c43f5f0f29c7f645118af1
duanyong <coderduan@gmail.com>
2017-05-29 01:38:31 +0800

    fixed: 由于excludes有可能为空，产生excludeList中包含空字符串的元素，造成bug.

--- smart-sso/smart-sso-client/src/main/java/com/smart/sso/client/ClientFilter.java
+++ smart-sso/smart-sso-client/src/main/java/com/smart/sso/client/ClientFilter.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -53,7 +54,7 @@ public abstract class ClientFilter implements Filter {
 			excludes = "";
 		}
 
-		excludeList = Arrays.asList(excludes.split(","));
+		excludeList = StringUtils.isBlank(excludes) ? new ArrayList<>() : Arrays.asList(excludes.split(","));
 	}
 
 	@Override
