8be5eae71933270d12f5b36781fb531e7eaec566
duanyong <coderduan@gmail.com>
2017-05-29 00:25:25 +0800

    修改ClientFilter.java，完成类似于https://github.com/a466350665/smart/issues/16的功能

--- smart-sso/smart-sso-client/src/main/java/com/smart/sso/client/ClientFilter.java
+++ smart-sso/smart-sso-client/src/main/java/com/smart/sso/client/ClientFilter.java
@@ -1,21 +1,5 @@
 package com.smart.sso.client;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.List;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.http.HttpStatus;
-
 import com.alibaba.fastjson.JSON;
 import com.smart.mvc.config.ConfigUtils;
 import com.smart.mvc.exception.ServiceException;
@@ -23,6 +7,15 @@ import com.smart.mvc.model.Result;
 import com.smart.mvc.util.SpringUtils;
 import com.smart.mvc.util.StringUtils;
 import com.smart.sso.rpc.AuthenticationRpcService;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 单点登录权限系统Filter基类
@@ -46,38 +39,48 @@ public abstract class ClientFilter implements Filter {
 		if (StringUtils.isBlank(ssoServerUrl = ConfigUtils.getProperty("sso.server.url"))) {
 			throw new IllegalArgumentException("ssoServerUrl不能为空");
 		}
+
 		if (StringUtils.isBlank(ssoAppCode = ConfigUtils.getProperty("sso.app.code"))) {
 			throw new IllegalArgumentException("ssoAppCode不能为空");
 		}
+
 		if ((authenticationRpcService = SpringUtils.getBean(AuthenticationRpcService.class)) == null) {
 			throw new IllegalArgumentException("authenticationRpcService注入失败");
 		}
 		
 		String excludes = filterConfig.getInitParameter("excludes");
-		if (StringUtils.isNotBlank(excludes)) {
-			excludeList = Arrays.asList(excludes.split(","));
+		if (StringUtils.isBlank(excludes)) {
+			excludes = "";
 		}
+
+		excludeList = Arrays.asList(excludes.split(","));
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
-			ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		if (excludeList != null && excludeList.contains(httpRequest.getServletPath()))
-			chain.doFilter(request, response);
-		else {
-			HttpServletResponse httpResponse = (HttpServletResponse) response;
-			try {
-				doFilter(httpRequest, httpResponse, chain);
+
+		for (String iginoreUrI : excludeList) {
+			//判断请求地址是否为以忽略地址为后缀，相同者略过检查
+			if (httpRequest.getServletPath().endsWith(iginoreUrI)) {
+				chain.doFilter(request, response);
+
+				return ;
 			}
-			catch (ServiceException e) {
-				httpResponse.setContentType("application/json;charset=UTF-8");
-				httpResponse.setStatus(HttpStatus.OK.value());
-				PrintWriter writer = httpResponse.getWriter();
-				writer.write(JSON.toJSONString(Result.create(e.getCode()).setMessage(e.getMessage())));
-				writer.flush();
-				writer.close();
-			}
+		}
+
+
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		try {
+			doFilter(httpRequest, httpResponse, chain);
+		} catch (ServiceException e) {
+			httpResponse.setContentType("application/json;charset=UTF-8");
+			httpResponse.setStatus(HttpStatus.OK.value());
+			PrintWriter writer = httpResponse.getWriter();
+			writer.write(JSON.toJSONString(Result.create(e.getCode()).setMessage(e.getMessage())));
+			writer.flush();
+			writer.close();
 		}
 	}
 
