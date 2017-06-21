f6236d6d6625160714b5730a5ae46fc99734bfc7
Joe <a466350665@qq.com>
2017-06-21 16:29:42 +0800

    跨域配置修改，允许额外的http请求头参数

--- smart-mvc/src/main/java/com/smart/mvc/filter/CrossOriginFilter.java
+++ smart-mvc/src/main/java/com/smart/mvc/filter/CrossOriginFilter.java
@@ -8,7 +8,10 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.smart.mvc.util.StringUtils;
 
 /**
  * 静态资源允许跨域处理，Nginx可在配置文件中指定
@@ -24,8 +27,13 @@ public class CrossOriginFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
 			throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
 		HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
 		httpResponse.addHeader("Access-Control-Allow-Origin", "*");
+		String acrh = httpRequest.getHeader("Access-Control-Request-Headers");
+		if (StringUtils.isNotBlank(acrh)) {
+			httpResponse.addHeader("Access-Control-Allow-Headers", acrh);
+		}
 		filterChain.doFilter(servletRequest, servletResponse);
 	}
 
