292ab073a96e3b44e69174265499e3c84cf063b2
zhoule <zhoule@daojia-inc.com>
2023-06-21 17:01:40 +0800

    Default Changelist

--- smart-sso-server/src/main/java/com/smart/sso/server/service/impl/PermissionServiceImpl.java
+++ smart-sso-server/src/main/java/com/smart/sso/server/service/impl/PermissionServiceImpl.java
@@ -1,14 +1,7 @@
 package com.smart.sso.server.service.impl;
 
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.smart.mvc.model.Condition;
 import com.smart.mvc.service.impl.ServiceImpl;
 import com.smart.sso.client.dto.RpcPermissionDto;
@@ -16,15 +9,33 @@ import com.smart.sso.server.common.Tree;
 import com.smart.sso.server.dao.PermissionDao;
 import com.smart.sso.server.dto.PermissionDto;
 import com.smart.sso.server.dto.TreeDto;
+import com.smart.sso.server.model.App;
 import com.smart.sso.server.model.Permission;
+import com.smart.sso.server.service.AppService;
 import com.smart.sso.server.service.PermissionService;
 import com.smart.sso.server.service.RolePermissionService;
+import com.smart.sso.server.service.UserRoleService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service("permissionService")
 public class PermissionServiceImpl extends ServiceImpl<PermissionDao, Permission> implements PermissionService {
 
 	@Autowired
 	private RolePermissionService rolePermissionService;
+	@Autowired
+	private UserRoleService userRoleService;
+	@Autowired
+	private AppService appService;
 
 	@Override
     public List<PermissionDto> selectTree(Integer appId, Integer roleId, Boolean isEnable) {
@@ -86,6 +97,27 @@ public class PermissionServiceImpl extends ServiceImpl<PermissionDao, Permission
 
 	@Override
 	public List<RpcPermissionDto> selectListByUserId(String appCode, Integer userId) {
-		return dao.selectListByUserId(appCode, userId);
+		App app = appService.selectByCode(appCode);
+		if (app == null || !app.getIsEnable()) {
+			return Collections.emptyList();
+		}
+		List<Permission> list = findByAppId(app.getId(), true);
+		if (userId != null) {
+			List<Integer> roleIdList = userRoleService.findRoleIdListByUserId(userId);
+			if (CollectionUtils.isEmpty(roleIdList)) {
+				return Collections.emptyList();
+			}
+			Set<Integer> permissionSet = Sets.newHashSet();
+			roleIdList.forEach(roleId -> permissionSet.addAll(rolePermissionService.findPermissionIdListByRoleId(roleId)));
+			if (CollectionUtils.isEmpty(permissionSet)) {
+				return Collections.emptyList();
+			}
+			list = list.stream().filter(t -> permissionSet.contains(t.getId())).collect(Collectors.toList());
+		}
+		return list.stream().map(t -> {
+			RpcPermissionDto dto = new RpcPermissionDto();
+			BeanUtils.copyProperties(t, dto);
+			return dto;
+		}).collect(Collectors.toList());
 	}
 }
