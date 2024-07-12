19c77b0371fe1170adbca59384f5a54fe16e70d4
zhoule <zhoule@daojia-inc.com>
2024-07-12 14:15:04 +0800

    去除多余注解

--- smart-sso-server/src/main/java/openjoe/smart/sso/server/ServerApplication.java
+++ smart-sso-server/src/main/java/openjoe/smart/sso/server/ServerApplication.java
@@ -2,9 +2,7 @@ package openjoe.smart.sso.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class ServerApplication {
 
