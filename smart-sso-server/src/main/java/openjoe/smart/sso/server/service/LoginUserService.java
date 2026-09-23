7b380e57b9b6a9ee606a4f275548c646b0458ced
zhoule <zhoule@daojia-inc.com>
2026-09-23 13:48:49 +0800

    升级smart-stage底座版本

--- smart-sso-server/src/main/java/openjoe/smart/sso/server/service/LoginUserService.java
+++ smart-sso-server/src/main/java/openjoe/smart/sso/server/service/LoginUserService.java
@@ -1,28 +1,71 @@
 package openjoe.smart.sso.server.service;
 
 import openjoe.smart.sso.server.dto.LoginUserDTO;
+import openjoe.smart.sso.server.entity.App;
+import openjoe.smart.sso.server.entity.TicketGrantingTicketContent;
+import openjoe.smart.sso.server.entity.User;
+import openjoe.smart.sso.server.manager.AbstractTicketGrantingTicketManager;
+import openjoe.smart.sso.server.manager.AbstractTokenManager;
 import openjoe.smart.stage.core.entity.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
-/**
- * 登录用户服务接口
- * 
- * @author Joe
- */
-public interface LoginUserService {
+@Service
+public class LoginUserService {
 
-	/**
-	 * 查询分页列表
-	 * @param account 登录名
-	 * @param name 姓名
-	 * @return
-	 */
-	Page<LoginUserDTO> selectPage(String account, String name, Long current, Long size);
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AppService appService;
+    @Autowired
+    private AbstractTicketGrantingTicketManager tgtManager;
+    @Autowired
+    private AbstractTokenManager tokenManager;
 
-	/**
-	 * 下线
-	 * @param tgtList
-	 */
-	void logout(List<String> tgtList);
+    public Page<LoginUserDTO> selectPage(String account, String name, Long current, Long size) {
+        Set<Long> userIds = null;
+        if (StringUtils.hasLength(account) || StringUtils.hasLength(name)) {
+            userIds = userService.selectUserIds(account, name);
+        }
+        Map<String, TicketGrantingTicketContent> tgtMap = tgtManager.getTgtMap(userIds, current, size);
+        List<LoginUserDTO> list = convertList(tgtMap);
+        return Page.of(current, size, list);
+    }
+
+    private List<LoginUserDTO> convertList(Map<String, TicketGrantingTicketContent> tgtMap) {
+        if (CollectionUtils.isEmpty(tgtMap)) {
+            return Collections.emptyList();
+        }
+        Map<Long, User> userMap = userService.selectMapByIds(
+                tgtMap.values().stream().map(TicketGrantingTicketContent::getUserId).collect(Collectors.toSet()));
+        Map<String, Set<String>> clientIdMap = tokenManager.getClientIdMapByTgt(tgtMap.keySet());
+        Map<String, App> appMap = appService.selectMapByClientIds(clientIdMap.values().stream()
+                .flatMap(Set::stream)
+                .collect(Collectors.toSet()));
+
+        List<LoginUserDTO> dtoList = new ArrayList<>();
+        tgtMap.forEach((tgt, tgtContent) -> {
+            LoginUserDTO dto = new LoginUserDTO();
+            dto.setTgt(tgt);
+            User user = userMap.get(tgtContent.getUserId());
+            dto.setId(user.getId());
+            dto.setName(user.getName());
+            dto.setAccount(user.getAccount());
+            dto.setCreateTime(new Date(tgtContent.getCreateTime()));
+            Set<String> clientIds = clientIdMap.get(tgt);
+            dto.setApps(clientIds.stream().map(
+                    clientId -> appMap.get(clientId).getCode()).collect(Collectors.joining(",")));
+            dtoList.add(dto);
+        });
+        return dtoList;
+    }
+
+    public void logout(List<String> tgtList) {
+        tgtList.forEach(tgt -> tgtManager.invalidate(tgt));
+    }
 }
--- smart-sso-server/src/main/java/openjoe/smart/sso/server/service/PermissionService.java
+++ smart-sso-server/src/main/java/openjoe/smart/sso/server/service/PermissionService.java
@@ -1,40 +1,126 @@
 package openjoe.smart.sso.server.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import openjoe.smart.sso.base.entity.TokenMenu;
+import openjoe.smart.sso.base.entity.TokenPermission;
 import openjoe.smart.sso.server.dto.PermissionDTO;
+import openjoe.smart.sso.server.dto.TreeDTO;
 import openjoe.smart.sso.server.entity.Permission;
+import openjoe.smart.sso.server.entity.Tree;
+import openjoe.smart.sso.server.manager.PermissionManager;
+import openjoe.smart.sso.server.mapper.PermissionMapper;
 import openjoe.smart.stage.mybatisplus.service.BaseService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
-/**
- * 权限服务接口
- * 
- * @author Joe
- */
-public interface PermissionService extends BaseService<Permission> {
+@Service
+public class PermissionService extends BaseService<PermissionMapper, Permission> implements PermissionManager {
 
-	/**
-	 * 根据名称和应用ID查询
-	 * @param appId 应用ID
-	 * @param roleId 角色ID
-	 * @param isEnable
-	 * @return
-	 */
-	List<PermissionDTO> selectTree(Long appId, Long roleId, Boolean isEnable);
+	@Autowired
+	private RolePermissionService rolePermissionService;
+	@Autowired
+	private UserRoleService userRoleService;
+
+    public List<PermissionDTO> selectTree(Long appId, Long roleId, Boolean isEnable) {
+        List<Permission> permissionList = findByAppId(appId, isEnable);
+        if (roleId == null) {
+            return addRoot(Tree.build(permissionList, r -> convertToDto(r, false)));
+        }
+
+        Set<Long> permissionIdSet = rolePermissionService.findPermissionIdSetByRoleIds(Collections.singletonList(roleId));
+        return addRoot(Tree.build(permissionList, r -> convertToDto(r, permissionIdSet.contains(r.getId()))));
+    }
 	
-	/**
-	 * 删除权限
-	 * @param id 权限ID
-	 * @param appId 应用ID
-	 * @return
-	 */
-	void delete(Long id, Long appId);
+	public List<PermissionDTO> addRoot(List<TreeDTO> list) {
+        PermissionDTO dto = new PermissionDTO();
+        dto.setName("根节点");
+        dto.setChildren(list);
+		List<PermissionDTO> dtoList = new ArrayList<>();
+		dtoList.add(dto);
+        return dtoList;
+    }
 	
-	/**
-	 * 删除应用下所有权限
-	 * @param idList 应用ID集合
-	 * @return
-	 */
-	void deleteByAppIds(Collection<Long> idList);
+	private PermissionDTO convertToDto(Permission r, Boolean checked) {
+        PermissionDTO dto = new PermissionDTO();
+        BeanUtils.copyProperties(r, dto);
+        dto.setChecked(checked);
+        return dto;
+    }
+	
+    private List<Permission> findByAppId(Long appId, Boolean isEnable) {
+		LambdaQueryWrapper<Permission> wrapper =  Wrappers.lambdaQuery();
+		wrapper.eq(appId != null, Permission::getAppId, appId);
+		wrapper.eq(isEnable != null, Permission::getIsEnable, isEnable);
+		wrapper.orderByDesc(Permission::getSort).orderByAsc(Permission::getId);
+		return list(wrapper);
+    }
+
+	@Transactional
+	public void delete(Long id, Long appId) {
+		List<Long> idList = new ArrayList<>();
+
+		List<Permission> list = findByAppId(appId, null);
+		loopSubList(id, idList, list);
+		idList.add(id);
+
+		rolePermissionService.deleteByPermissionIds(idList);
+
+		removeByIds(idList);
+	}
+
+	// 递归方法，删除子权限
+	protected void loopSubList(Long id, List<Long> idList, List<Permission> list) {
+		for (Permission p : list) {
+			if (id.equals(p.getParentId())) {
+				idList.add(p.getId());
+				loopSubList(p.getId(), idList, list);
+			}
+		}
+	}
+
+	public void deleteByAppIds(Collection<Long> idList) {
+		LambdaQueryWrapper<Permission> wrapper =  Wrappers.lambdaQuery();
+		wrapper.in(Permission::getAppId, idList);
+		remove(wrapper);
+	}
+
+	@Override
+	public TokenPermission getUserPermission(Long userId, Long appId) {
+		List<Permission> list = findByAppId(appId, true);
+		if(CollectionUtils.isEmpty(list)){
+			return new TokenPermission(Collections.emptySet(), Collections.emptySet(), Collections.emptyList());
+		}
+		Set<Long> permissionIdSet = getUserPermissionIdSet(userId);
+		Set<String> permissionSet;
+		Set<String> noPermissionSet;
+		if (CollectionUtils.isEmpty(permissionIdSet)) {
+			permissionSet = Collections.emptySet();
+			noPermissionSet = list.stream().map(t -> t.getUrl()).collect(Collectors.toSet());
+		}
+		else{
+			permissionSet = list.stream().filter(t -> permissionIdSet.contains(t.getId())).map(t -> t.getUrl()).collect(Collectors.toSet());
+			noPermissionSet= list.stream().filter(t -> !permissionIdSet.contains(t.getId())).map(t -> t.getUrl()).collect(Collectors.toSet());
+		}
+		List<TokenMenu> menuList = list.stream().filter(t -> t.getIsMenu() && !noPermissionSet.contains(t.getUrl())).map(t->{
+			TokenMenu dto = new TokenMenu();
+			BeanUtils.copyProperties(t, dto);
+			return dto;
+		}).collect(Collectors.toList());
+		return new TokenPermission(permissionSet, noPermissionSet, menuList);
+	}
+
+	private Set<Long> getUserPermissionIdSet(Long userId){
+		List<Long> roleIdList = userRoleService.findRoleIdListByUserId(userId);
+		if (CollectionUtils.isEmpty(roleIdList)) {
+			return Collections.emptySet();
+		}
+		return rolePermissionService.findPermissionIdSetByRoleIds(roleIdList);
+	}
 }
